246d631e55ec5133a2459db50e7a620d733e297e
tbonza <tylers.pile@gmail.com>
2020-07-28 15:29:11 -0400

    collecting articles directly from newspapers

--- dpipe/collect.py
+++ dpipe/collect.py
@@ -1,104 +1,66 @@
 from datetime import datetime
-import json
-import os
-import re
 import logging
-from urllib.parse import urljoin
 
-from lxml import html
-import requests
+import newspaper
 
 logger = logging.getLogger(__name__)
 
-def parse_topic_titles(tree:html.HtmlElement) -> list:
-    return tree.xpath('//span[@class="toctext"]/text()') 
+def enable_log(log_name):
+    """ Enable logs written to file """
+    logging.basicConfig(filename= log_name, level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
-def parse_major_topics(tree:html.HtmlElement) -> list:
-    return tree.xpath('//h2/span[@class="mw-headline"]/text()')
+def identify_articles(newspaper_urls:list, keywords:set):
+    """ Identify any articles containing keywords in urls. """
+    for newsurl in newspaper_urls:
+        
+        paper = newspaper.build(newsurl)
 
-def parse_minor_topics(tree:html.HtmlElement) -> list:
-    return tree.xpath('//h3/span[@class="mw-headline"]/text()') 
+        keypaper = []
+        for article in paper.articles:
 
-def orient_topics(tree:html.HtmlElement) -> dict:
-    titles = parse_topic_titles(tree)
-    majt = parse_major_topics(tree)
-    mint = parse_minor_topics(tree)
+            for keyword in keywords:
 
-    if len(titles) == 0 or len(majt) == 0 or len(mint) == 0:
-        return {}
+                if keyword in article.url:
+                    yield article
 
-    tt = {}
-    current = majt[0]
-    majt = set(majt)
-    mint = set(mint)
-    for cand in titles:
-        logger.info(cand)
-        if cand in majt:
+def process_article(article):
+    doc = {}
+    doc['url'] = article.url
 
-            tt[cand] = []
+    article.download()
+    article.parse()
+    article.nlp()
 
-            if cand != current:
-                current = cand
+    doc['authors'] = article.authors
+    doc['publish_date'] = str(article.publish_date)
+    doc['text'] = article.text
+    doc['keywords'] = article.keywords
 
-        if cand in mint:
+    return doc
 
-            tt[current].append(cand)
+def store_articles(fpath:str, newspaper_urls:list, keywords:set):
 
-    return tt
+    dailies = []
+    for article in identify_articles(newspaper_urls, keywords):
 
-def cache_or_fetch(url:str, fpath:str):
+        parsed = process_article(article)
+        dailies.append(parsed)
+        logger.info("Processed. {}".format(article.url))
 
-    if os.path.exists(fpath):
-        with open(fpath, "rb") as f:
-            r_content = f.read()
-        tree = html.fromstring(r_content)
-        return 0, tree
-
-    r = requests.get(url)
-    if r.status_code == requests.codes.ok:
-       
-        with open(fpath, "wb") as f:
-            f.write(r.content)
-
-        tree = html.fromstring(r.content)
-        return 0, tree
-
-    return r.status_code, ""
-
-def output_candidates(urls:list, cachedir:str) -> dict:
-    
-    output = {}
-    for url in urls:
-
-        logger.info("Processing. {}".format(url))
-        fname = url.split("/")[-1]
-        dt = "{}{}".format(datetime.now().year, datetime.now().month)
-        fname += dt
-        fpath = urljoin(cachedir, fname)
-
-        errcode, tree = cache_or_fetch(url, fpath)
-        if errcode == 0:
-
-            topics = orient_topics(tree)
-            output[url] = topics
-
-        else:
-            logger.error("bad status code. {}. {}".format(errcode, url))
-
-    return output
-
+    with open(fpath, "w") as f:
+        json.dump(dailies, f)
+        logger.info("Wrote out. {}".format(fpath))
 
 if __name__ == "__main__":
 
-    cachedir = "/Users/tylerbrown/Downloads/wiki/"
-    urls = [
-        "https://en.wikipedia.org/wiki/Political_positions_of_Joe_Biden",
-        "https://en.wikipedia.org/wiki/Political_positions_of_Kamala_Harris",
-        "https://en.wikipedia.org/wiki/Political_positions_of_Donald_Trump",
-        "https://en.wikipedia.org/wiki/Political_positions_of_the_2020_Democratic_Party_presidential_primary_candidates",
+    dt = datetime.now()
+    fpath = "/Users/tylerbrown/Downloads/articles/daily_articles_{}-{}-{}.json".format(dt.year, dt.month, dt.day)
+    newspaper_urls = [
+        "https://www.cnn.com/",
+        "https://www.foxnews.com/",
+        "https://www.nytimes.com/"
     ]
-    output = output_candidates(urls, cachedir)
-    op = "candidate_topics_{}{}.json".format(datetime.now().year, datetime.now().month)
-    outpath = urljoin(cachedir, op)
-    with open(outpath, "w") as outfile:
-        json.dump(output, outfile)
+    keywords = set(['trump','biden'])
+    
+    enable_log("collect_daily_articles.log")
+    store_articles(fpath, newspaper_urls, keywords)
