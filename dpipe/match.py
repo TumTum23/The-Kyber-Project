56ee8a6baaed6f4277b06241c35119ec54d986b6
tbonza <tylers.pile@gmail.com>
2020-07-27 21:56:42 -0400

    seeing very little overlap between topics listed on wikipedia for candidates

--- dpipe/match.py
+++ dpipe/match.py
@@ -1,23 +1,22 @@
 from itertools import combinations
 
-def extract_values(top_level:list):
+def extract_values(d:dict):
     """ We only want to compare lower level topics. """
-    low_level = []
-    for v in top_level:
-        low_level.extend(v)
-    return low_level
+    h = [] 
+    for k in d.keys(): 
+        s = [] 
+        for j,v in d[k].items(): 
+            s.extend(v) 
+        h.append((k, set(s)))
+    return h
 
 def jaccard(d:dict, r=2):
     """ Find the jaccard distance for all possible combinations. 
 
     Rough measure to assess coverage of similar topics across candidates.
     """
-    sim = []
-    for cand0, cand1 in list(combinations(d.keys(),r)):
-
-        c0 = set(extract_values(d[cand0]))
-        c1 = set(extract_values(d[cand1]))
-
-        sim.append((cand0, cand1, len(c0 & c1) / len(c0 | c1), len(c0 | c1)))
-
-    return sim
+    k = []
+    h = extract_values(d)
+    for m,n in combinations([i for i in range(len(h))], r):
+        k.append((h[m][0], h[n][0], len(h[m][1] & h[n][1]) / len(h[m][1] | h[n][1])))
+    return k
