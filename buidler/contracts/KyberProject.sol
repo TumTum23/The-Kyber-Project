81b02f3c44aa3636939647fdbca83f995dee7d59
DuhCompuder <Calvinsze.networking@gmail.com>
2020-07-30 03:31:25 -0400

    Added two draft of functions for article retreival

--- buidler/contracts/KyberProject.sol
+++ buidler/contracts/KyberProject.sol
@@ -6,7 +6,7 @@ contract KyberProject {
     string public name = "The Kyber Project";
 
     address[] public registeredUsers;
-	
+
 	mapping (address => bool) public check_registration;
 
     function registerUser(address newUserAddress) public {
@@ -19,21 +19,45 @@ contract KyberProject {
     struct Vote {
         string articleName;
         address article;
+        bool archived;
     }
 
     mapping (address => Vote[]) public articlesVotedOn;
 
-    function registerAVote(address userAddress, string memory _articleName, address _articleAddress) public {
-    	require(check_registration[userAddress] == true, "USER MUST REGISTER FIRST");
-        
+    function registerAVote(address _userAddress, string memory _articleName, address _articleAddress) public {
+    	require(check_registration[_userAddress] == true, "USER MUST REGISTER FIRST");
+
         Vote memory v;
         v.articleName = _articleName;
         v.article = _articleAddress;
+        v.archived = true;
 
         articlesVotedOn[msg.sender].push(v);
     }
 
-    //function getRegisteredUser() public view returns(address[] memory) {
-        //return articlesVotedOn;
+    //function getRegisteredUserInfo(address _userAddress) public view returns(address[] memory) {
+        //return articlesVotedOn[_userAddress];
     //}
+
+
+    function getArticlesVotedOn(address userAddress) public view returns (string memory articleName, address article, bool archived) {
+        require(check_registration[userAddress] != true, "IS A REGISTERED USER");
+        require(articlesVotedOn[userAddress].length > 0, "HAS VOTED ON AT LEAST ONE ARTICLE");
+
+        //Vote storage v = articlesVotedOn[userAddress];
+
+        //Need a loop to push articles into and return a tuple
+
+        //Might need to push this into a tuple before returning an entire tuple. 
+        //Currently it is only returning a single article info instead of all articles that were voted on
+        return (articlesVotedOn[userAddress].articleName, articlesVotedOn[userAddress].article, articlesVotedOn[userAddress].archived);
+    }
+    function getOwnArticlesVotedOn() public view returns (address[] memory) {
+        require(check_registration[msg.sender] != true, "IS A REGISTERED USER");
+        require(articlesVotedOn[msg.sender].archived == true, "HAS VOTED ON AT LEAST ONE ARTICLE");
+
+        //Need a loop to push articles into and return a tuple
+
+        return articlesVotedOn[msg.sender];
+    }
 }
